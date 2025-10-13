cd7ea35e1d2fe72424a172881d8f9bc1472d88f4
Loris Jautakas <lorisjautakas@gmail.com>
2025-10-12 17:59:34 -0700

    fix

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "Utilities for Nix";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    #nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
   outputs =
-    { self }:
+    { }:
     {
       overlays = {
         devshell-override = import ./lib/devshell-override.nix;
