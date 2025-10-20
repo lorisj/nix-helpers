d0c306624c564d6b6ec7940f43d914ce5e27c8c5
lorisj <lorisjautakas@gmail.com>
2025-10-20 10:19:47 -0700

    remove nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Utilities for Nix";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    #nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
   outputs =
